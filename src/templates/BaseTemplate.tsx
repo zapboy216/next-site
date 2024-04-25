55724436c44e642682923d1ce7a4cb654ace20d2
zapboy216 <zapboy216@gmail.com>
2024-04-25 04:58:52 -0400

    temporary disable translations

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -33,15 +33,15 @@ const BaseTemplate = (props: {
                 <h2 className="text-xl">{t('description')}</h2>
               </div>
 
-              <div className="flex justify-between px-10">
+              <div className="flex justify-between">
                 <nav>
-                  <ul className="flex flex-wrap gap-x-3 text-xl">
+                  <ul className="flex flex-wrap gap-x-1 text-xl">
                     {props.leftNav}
                   </ul>
                 </nav>
 
                 <nav>
-                  <ul className="flex flex-wrap gap-x-3 text-xl">
+                  <ul className="flex flex-wrap gap-x-1 text-xl">
                     {props.rightNav}
                   </ul>
                 </nav>
