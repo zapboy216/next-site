3255f3d5feab4d929536e12a192f411bfa5813ca
zapboy216 <zapboy216@gmail.com>
2024-04-29 09:28:34 -0400

    update workflows

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,9 +1,3 @@
-'use client';
-
-import '@copilotkit/react-ui/styles.css';
-
-import { CopilotKit } from '@copilotkit/react-core';
-import { CopilotSidebar } from '@copilotkit/react-ui';
 import { useTranslations } from 'next-intl';
 
 import { AppConfig } from '@/utils/AppConfig';
@@ -17,41 +11,32 @@ const BaseTemplate = (props: {
 
   return (
     <div className="w-full px-20 py-10 text-gray-700 antialiased">
-      <CopilotKit url="/api/copilotkit/">
-        <CopilotSidebar
-          labels={{
-            title: 'Your Assistant',
-            initial: 'Hi! 👋 How can I assist you today?',
-          }}
-        >
-          <div className="mx-auto max-w-screen-md lg:max-w-full">
-            <header className="border-b border-gray-300 px-10">
-              <div className="pt-8">
-                <h1 className="text-3xl font-bold text-gray-900">
-                  {AppConfig.name}
-                </h1>
-                <h2 className="text-xl">{t('description')}</h2>
-              </div>
-
-              <div className="flex justify-between">
-                <nav>
-                  <ul className="flex flex-wrap gap-x-1 text-xl">
-                    {props.leftNav}
-                  </ul>
-                </nav>
-
-                <nav>
-                  <ul className="flex flex-wrap gap-x-1 text-xl">
-                    {props.rightNav}
-                  </ul>
-                </nav>
-              </div>
-            </header>
-
-            <main>{props.children}</main>
+      <div className="mx-auto max-w-screen-md lg:max-w-full">
+        <header className="border-b border-gray-300 px-10">
+          <div className="pt-8">
+            <h1 className="text-3xl font-bold text-gray-900">
+              {AppConfig.name}
+            </h1>
+            <h2 className="text-xl">{t('description')}</h2>
           </div>
-        </CopilotSidebar>
-      </CopilotKit>
+
+          <div className="flex justify-between">
+            <nav>
+              <ul className="flex flex-wrap gap-x-1 text-xl">
+                {props.leftNav}
+              </ul>
+            </nav>
+
+            <nav>
+              <ul className="flex flex-wrap gap-x-1 text-xl">
+                {props.rightNav}
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <main>{props.children}</main>
+      </div>
     </div>
   );
 };
